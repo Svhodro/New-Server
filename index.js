573e480e9759e00b4b01a559858e7016b261ab8c
Svhodro <rudrolipi@gmail.com>
2024-04-30 02:15:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+const express=require("express")
+const cors=require('cors')
+const app=express()
+require('dotenv').config()
+const port=process.env.PORT || 5000
+
+//middelware
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('running the server')
+})
+
+//mongodb
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mj0igoj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    //collection 
+    const database=client.db('Assingment').collection('tourist') 
+    //create
+    app.get('/insert',async(req,res)=>{
+      data={name:"sporsho",
+            job:"sorkari",
+            index:"data"
+    }
+      const result=await database.insertOne(data)
+      res.send(result)
+    }) 
+    //read all
+    app.get('/Get',async(req,res)=>{
+     
+      const result=  database.find()
+      const resul=await result.toArray()
+     res.send(resul)
+    })
+    //read one
+    app.get('/one',async(req,res)=>{
+     
+      const result= await database.findOne({
+        name:
+        "sporsho"})      
+       res.send(result)
+       console.log(result)
+    })
+// delete
+app.get('/Delete',async(req,res)=>{
+     
+  const result= await database.deleteOne({
+    name:"sporsho"})      
+   res.send(result)
+   console.log(result)
+})
+//update
+app.get('/Update',async(req,res)=>{  
+  const filter = { name:"sporsho" }
+  const update={
+    $set:{
+      name:"rudro vhodro",
+      job:"besorkari"
+    }
+  }
+  const options = { upsert: true };
+  const result= await database.updateOne(filter,update,options)      
+   res.send(result)
+   console.log(result)
+})
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir); 
+
+app.listen(port,()=>{
+    console.log("server is running")
+})
