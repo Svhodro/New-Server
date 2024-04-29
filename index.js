cff8d8f3a48b8b64f1d1c28ba458a89b5e9820d6
Svhodro <rudrolipi@gmail.com>
2024-04-30 02:25:47 +0600

    first commit  dsd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const cors=require('cors')
 const app=express()
 require('dotenv').config()
-const port=process.env.PORT || 5000
+const port= 5000
 
 //middelware
 app.use(cors())
